4147e4bdaae7dcc65b7fc88a7e4a90a978a95620
ManigandanMJ <134141272+ManigandanMJ@users.noreply.github.com>
2023-07-20 20:24:29 +0530

    mountpoint created

--- Assignments/Adf_Assignment.py
+++ Assignments/Adf_Assignment.py
@@ -1,1 +1,9 @@
 # Databricks notebook source
+#creating mount point
+dbutils.fs.mount(source = 'wasbs://manidbcontainer@manidb1azure.blob.core.windows.net',
+mount_point = '/mnt/adfassignment',
+extra_configs = {'fs.azure.account.key.manidb1azure.blob.core.windows.net':'X96T9Vc26NOxHA2ZnMomDDDLtPhUeGwNXub8cFQ1cOZgSDJNk5yHdKE/h1hIZjtawWRb2gozVuCH+AStZlT6sw=='}
+)
+
+# COMMAND ----------
+
