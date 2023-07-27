4f2a4659e1b1ccfc6b60728641881c5755e02a0e
ManigandanMJ <134141272+ManigandanMJ@users.noreply.github.com>
2023-07-27 17:48:26 +0530

    Merge functions updated

--- Assignments/Adf_Assignment.py
+++ Assignments/Adf_Assignment.py
@@ -1,4 +1,5 @@
 # Databricks notebook source
+
 #creating mount point
 dbutils.fs.mount(source = 'wasbs://adfcontainer@manidb1azure.blob.core.windows.net',
 mount_point = '/mnt/adf_assignment',
@@ -6,6 +7,7 @@ extra_configs = {'fs.azure.account.key.manidb1azure.blob.core.windows.net':'X96T
 )
 
 # COMMAND ----------
+
 
 # reading the open api file json
 import requests
@@ -18,9 +20,11 @@ display(df)
 
 # COMMAND ----------
 
+
 df.write.json('/mnt/adf_assignment/bronze/animals.json')
 
 # COMMAND ----------
+
 
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType, ArrayType
@@ -44,9 +48,72 @@ exploded_df.write.json('/mnt/adf_assignment/bronze/animals_jdf.json')
 
 # COMMAND ----------
 
+
 #reading the data from bronze layer
 adf_df = spark.read.option('header', True).json('/mnt/adf_assignment/bronze/animals_jdf.json')
 display(adf_df)
+
+# COMMAND ----------
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, ArrayType
+from pyspark.sql.functions import explode
+
+schema = StructType([
+    StructField("count",IntegerType(), True),
+    StructField("entries",ArrayType(StructType([
+        StructField("API", StringType(), True),
+        StructField("Auth", StringType(), True),
+        StructField("Category", StringType(), True),
+        StructField("Cors", StringType(), True),
+        StructField("Description", StringType(), True),
+        StructField("HTTPS", StringType(), True),
+        StructField("Link", StringType(), True)])), True)])
+data =  [(1425,[("AdoptAPet","apiKey","Animals","yes","Resource to help get pets adopted","true","https://www.adoptapet.  com/public/apis/pet_list.html")]),
+         (1426,[("AdoptAPet","apiKey","Animals","yes","Resource to help get pets adopted","true","https://www.adoptapet.  com/public/apis/pet_list.html")])]
+update_df = spark.createDataFrame(data=data, schema=schema)
+display(update_df)
+
+
+# COMMAND ----------
+
+update_df.createOrReplaceTempView("source_view")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from source_view
+
+# COMMAND ----------
+
+from delta.tables import * 
+update_df.write.format("delta").mode("overwrite").option('path','/mnt/adf_assignment/gold').saveAsTable("animals_delta")
+
+# COMMAND ----------
+
+
+update_df.alias("target").merge(
+    source=adf_df.alias("source"),
+    condition="target.count = source.count"
+).whenMatchedUpdate(set=
+                    {
+                    "API": "source.API",
+                    "Auth": "source.Auth",
+                    "Category": "source.Category",
+                    "Description": "source.Description",
+                    "HTTPS": "source.HTTPS",
+                    "Link": "source.Link"
+                    }
+                    ).whenNotMatchedInsert(values = 
+                    {                                              
+                    "count": "source.count",
+                    "API": "source.API",
+                    "Auth": "source.Auth",
+                    "Category": "source.Category",
+                    "Description": "source.Description",
+                    "HTTPS": "source.HTTPS",
+                    "Link": "source.Link" 
+                     }).execute()
 
 # COMMAND ----------
 
